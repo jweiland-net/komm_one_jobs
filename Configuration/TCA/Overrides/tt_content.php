66cfab65506845e2b26699cf57f0d0d4cda049df
Stefan Froemken <sfroemken@jweiland.net>
2025-07-18 13:24:44 +0200

    Use short syntax for namespaces

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,5 +1,8 @@
 <?php
 
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+
 if (!defined('TYPO3')) {
     die('Access denied.');
 }
@@ -7,12 +10,12 @@ if (!defined('TYPO3')) {
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['kommonejobs_job'] = 'layout,select_key,pages,recursive';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['kommonejobs_job'] = 'pi_flexform';
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+ExtensionManagementUtility::addPiFlexFormValue(
     'kommonejobs_job',
     'FILE:EXT:komm_one_jobs/Configuration/Flexforms/Jobs.xml'
 );
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+ExtensionUtility::registerPlugin(
     'KommOneJobs',
     'Job',
     'Komm.ONE Jobs'
